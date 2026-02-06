f1ea9e4e52147a1d0243de808651a9fddec192f7
Deanna Vazquez <deanna.vazquez@kin.com>
2026-02-06 11:36:03 -0600

    implement checking guess vs secret and add loop 1-12

--- lib/code.rb
+++ lib/code.rb
@@ -7,6 +7,7 @@ class Code
     welcome_message
     @secret = generate_code
     player_guess
+    p @secret
   end
 
   def welcome_message
@@ -34,15 +35,21 @@ class Code
   end
 
   def player_guess
-    puts '=== Starting player_guess ==='
-    puts "Enter your guess (4 letters) using these colors: #{ALLOWED_COLORS}"
-    input = gets.chomp
-    guess = input.upcase.split(//)
-    if valid_choice?(guess)
-      check_color_matches(@secret, guess)
-    else
-      puts 'INVALID MOVE!'
-      player_guess
+    i = 1
+    loop do
+      break if i > 12 || @secret == guess
+
+      puts "=== Starting player_guess #{i}/12 ==="
+      puts "Enter your guess (4 letters) using these colors: #{ALLOWED_COLORS}"
+      input = gets.chomp
+      guess = input.upcase.split(//)
+      if valid_choice?(guess)
+        check_color_matches(@secret, guess)
+        i += 1
+      else
+        puts 'INVALID MOVE!'
+        player_guess
+      end
     end
   end
 
@@ -52,19 +59,28 @@ class Code
   end
 
   def check_color_matches(secret, guess)
-    secret.each_with_index do |col1, i|
-      if col1 == guess[i]
-        puts 'exact match'
-      elsif secret.include?(guess[i])
+    guess_dup = guess.dup
+    secret_dup = secret.dup
+
+    secret_dup.dup.each_with_index do |col1, i|
+      next unless col1 == guess_dup[i]
+
+      puts "exact match at position #{i}"
+      secret_dup[i] = nil
+      guess_dup[i] = nil
+    end
+
+    guess_dup.each_with_index do |col, i|
+      next if col.nil?
+
+      if secret_dup.include?(col)
         puts 'color match'
+        secret_dup[secret_dup.index(col)] = nil
       else
-        puts 'no match' # Compare secret at position c1 with guess at same position
+        puts 'no match'
       end
     end
   end
 end
 
 Code.new
-# p code.generate_code
-# p code.player_guess
-# p code.check_color_matches(%w[R G B Y], %w[R B G Y])
